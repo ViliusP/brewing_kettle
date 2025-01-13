afc7506ceae736d0132ab1a14089e0ea557dc29c
ViliusP <viliuspaliokas@gmail.com>
2025-01-13 16:37:47 +0200

    Add more fonts

--- esp/components/ui/font/symbols.h
+++ esp/components/ui/font/symbols.h
@@ -14,7 +14,7 @@
 #define WIFI_ALERT "\xF3\xB1\x9A\xB5"
 
 // message_outline 0xF0365
-#define MESSAGe_OUTLINE_SYMBOL "\xF3\xB0\x8D\xA5" 
+#define MESSAGE_OUTLINE_SYMBOL "\xF3\xB0\x8D\xA5" 
 
 // message_reply_outline 0xF173D
 #define MESSAGE_REPLY_OUTLINE_SYMBOL "\xF3\xB1\x9C\xBD"
