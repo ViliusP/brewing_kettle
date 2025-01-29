bb3e3bb34f5d5cd0d28240aec62fc9c80cd61622
ViliusP <viliuspaliokas@gmail.com>
2025-01-29 12:09:02 +0200

    Change uart messaging

--- kettle_heating/components/uart_communication/include/uart_communication.h
+++ kettle_heating/components/uart_communication/include/uart_communication.h
@@ -4,11 +4,28 @@
 #include "driver/uart.h"
 
 #define RX_BUF_SIZE 2048
+#define MESSAGE_START_BYTE 0x02 // STX
+#define MESSAGE_END_BYTE 0x03   // ETX
+#define MAX_MESSAGE_LENGTH 256
 
-typedef void (*rx_task_callback_t)(const char *, const int);
+// Define message commands using an enum
+typedef enum
+{
+    CMD_SEND_SENSOR_DATA = 0x01,
+    CMD_SET_VALUE = 0x02,
+} uart_message_command_t;
 
-int uart_send_data(const char *data);
-int uart_send_uint32(uint32_t value);
+// Define message structure
+typedef struct
+{
+    uart_message_command_t command;
+    uint8_t data[MAX_MESSAGE_LENGTH];
+    uint8_t data_len;
+} uart_message_t;
+
+typedef void (*rx_task_callback_t)(const char *data, int len);
+
+int uart_send_message(uart_message_t *msg);
 
 void initialize_uart(uart_config_t uart_config, int tx_pin, int rx_pin);
 void start_uart_task(rx_task_callback_t rx_task_callback);
