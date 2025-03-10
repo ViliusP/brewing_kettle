657260b77bea7665bac30d03d11ec5f6979d3701
ViliusP <viliuspaliokas@gmail.com>
2025-03-10 16:36:13 +0200

    Change PWM frequency to 60hz

--- kettle_heating/components/peripherals/include/peripherals.h
+++ kettle_heating/components/peripherals/include/peripherals.h
@@ -6,7 +6,7 @@
 #define LEDC_MODE LEDC_LOW_SPEED_MODE
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 #define LEDC_RESOLUTION LEDC_TIMER_13_BIT    // 8192 steps
-#define PWM_FREQUENCY 4000                   // 1Hz (for AC SSR) or higher for DC SSR
+#define PWM_FREQUENCY 60                  
 #define MAX_DUTY (1 << LEDC_RESOLUTION) - 1; // 8191
 
 esp_err_t set_ssr_duty(float duty_percent);
