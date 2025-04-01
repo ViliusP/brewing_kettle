d041d6eb21adfc24a43f6baafc8518a40f7e8d08
ViliusP <viliuspaliokas@gmail.com>
2025-04-01 11:15:51 +0300

    Expand and add equals and hashCode to SystemInfo classes

--- web/lib/core/data/models/websocket/device_configuration.dart
+++ web/lib/core/data/models/websocket/device_configuration.dart
@@ -2,13 +2,30 @@ part of 'inbound_message.dart';
 
 class SystemInfo extends WsInboundMessagePayload {
   final DeviceInfo communicator;
-  final DeviceInfo heater;
+  final HeaterDeviceInfo heater;
 
   const SystemInfo({required this.communicator, required this.heater});
 
   factory SystemInfo.fromJson(Map<String, dynamic> json) {
-    return SystemInfo(heater: DeviceInfo.empty(), communicator: DeviceInfo.fromJson(json));
+    var heaterJson = json["heater"];
+    var communicatorJson = json["communicator"];
+
+    return SystemInfo(
+      heater: HeaterDeviceInfo.fromJson(heaterJson),
+      communicator: DeviceInfo.fromJson(communicatorJson),
+    );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SystemInfo &&
+          runtimeType == other.runtimeType &&
+          communicator == other.communicator &&
+          heater == other.heater;
+
+  @override
+  int get hashCode => Object.hash(communicator, heater);
 }
 
 class DeviceInfo {
@@ -37,6 +54,54 @@ class DeviceInfo {
 
     return DeviceInfo(hardware: hardwareInfo, software: softwareInfo);
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DeviceInfo &&
+          runtimeType == other.runtimeType &&
+          hardware == other.hardware &&
+          software == other.software;
+
+  @override
+  int get hashCode => Object.hash(hardware, software);
+}
+
+class HeaterDeviceInfo implements DeviceInfo {
+  @override
+  final DeviceHardwareInfo hardware;
+
+  @override
+  final HeaterDeviceSoftwareInfo software;
+
+  const HeaterDeviceInfo({required this.software, required this.hardware});
+
+  factory HeaterDeviceInfo.fromJson(Map<String, dynamic> json) {
+    var hardwareJson = json["hardware"];
+    var hardwareInfo = const DeviceHardwareInfo();
+    if (hardwareJson is Map<String, dynamic>) {
+      hardwareInfo = DeviceHardwareInfo.fromJson(hardwareJson);
+    }
+
+    var softwareJson = json["software"];
+    var softwareInfo = HeaterDeviceSoftwareInfo();
+    if (softwareJson is Map<String, dynamic>) {
+      softwareInfo = HeaterDeviceSoftwareInfo.fromJson(softwareJson);
+    }
+
+    return HeaterDeviceInfo(hardware: hardwareInfo, software: softwareInfo);
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HeaterDeviceInfo &&
+          runtimeType == other.runtimeType &&
+          hardware == other.hardware &&
+          software == other.software;
+
+  @override
+  int get hashCode => Object.hash(hardware, software);
 }
 
 class DeviceHardwareInfo {
@@ -91,6 +156,21 @@ class DeviceHardwareInfo {
       minimumHeapSize: minimumHeapSize,
     );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DeviceHardwareInfo &&
+          runtimeType == other.runtimeType &&
+          chip == other.chip &&
+          cores == other.cores &&
+          features == other.features &&
+          siliconRevision == other.siliconRevision &&
+          flash == other.flash &&
+          minimumHeapSize == other.minimumHeapSize;
+
+  @override
+  int get hashCode => Object.hash(chip, cores, features, siliconRevision, flash, minimumHeapSize);
 }
 
 class DeviceSoftwareInfo {
@@ -133,6 +213,20 @@ class DeviceSoftwareInfo {
       compileTime: "$compileDate $compileTime",
     );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DeviceSoftwareInfo &&
+          runtimeType == other.runtimeType &&
+          version == other.version &&
+          secureVersion == other.secureVersion &&
+          idfVersion == other.idfVersion &&
+          projectName == other.projectName &&
+          compileTime == other.compileTime;
+
+  @override
+  int get hashCode => Object.hash(version, secureVersion, idfVersion, projectName, compileTime);
 }
 
 class FlashInfo {
@@ -152,6 +246,69 @@ class FlashInfo {
 
     return FlashInfo(size: size, type: type);
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FlashInfo &&
+          runtimeType == other.runtimeType &&
+          size == other.size &&
+          type == other.type;
+
+  @override
+  int get hashCode => Object.hash(size, type);
+}
+
+class HeaterDeviceSoftwareInfo extends DeviceSoftwareInfo {
+  final PidConstants? pidConstants;
+
+  const HeaterDeviceSoftwareInfo({
+    this.pidConstants,
+    super.projectName,
+    super.version,
+    super.secureVersion,
+    super.idfVersion,
+    super.compileTime,
+  });
+
+  factory HeaterDeviceSoftwareInfo.fromJson(Map<String, dynamic> json) {
+    final String? projectName = json["project_name"];
+    final String? version = json["version"];
+    final int? secureVersion = json["secure_version"];
+    final String? idfVersion = json["idf_version"];
+    final String? compileTime = json["compile_time"];
+    final String? compileDate = json["compile_date"];
+
+    PidConstants? pidConstants;
+    if (json["pid_constants"] is Map<String, dynamic>) {
+      pidConstants = PidConstants.fromJson(json["pid_constants"] as Map<String, dynamic>);
+    }
+
+    return HeaterDeviceSoftwareInfo(
+      projectName: projectName,
+      version: version,
+      secureVersion: secureVersion,
+      idfVersion: idfVersion,
+      compileTime: "$compileDate $compileTime",
+      pidConstants: pidConstants,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HeaterDeviceSoftwareInfo &&
+          runtimeType == other.runtimeType &&
+          version == other.version &&
+          secureVersion == other.secureVersion &&
+          idfVersion == other.idfVersion &&
+          projectName == other.projectName &&
+          compileTime == other.compileTime &&
+          pidConstants == other.pidConstants;
+
+  @override
+  int get hashCode =>
+      Object.hash(version, secureVersion, idfVersion, projectName, compileTime, pidConstants);
 }
 
 enum ChipFeatures {
