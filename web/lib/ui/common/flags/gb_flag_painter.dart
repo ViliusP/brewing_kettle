b516da5ce93ec2075a7eba44f66380e4fbff75fb
ViliusP <viliuspaliokas@gmail.com>
2025-04-22 12:28:56 +0300

    Initial GB flag fix

--- web/lib/ui/common/flags/gb_flag_painter.dart
+++ web/lib/ui/common/flags/gb_flag_painter.dart
@@ -1,18 +1,46 @@
+import 'package:brew_kettle_dashboard/utils/painting_utilities.dart';
 import 'package:flutter/rendering.dart';
 
 class GbFlagPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    Path path0 = Path();
-    path0.moveTo(0, 0);
-    path0.lineTo(0, size.height);
-    path0.lineTo(size.width, size.height);
-    path0.lineTo(size.width, 0);
-    path0.close();
+    final Rect canvasRect = Rect.fromLTWH(0, 0, size.width, size.height);
 
-    Paint paint0Fill = Paint()..style = PaintingStyle.fill;
-    paint0Fill.color = Color(0xff012169).withOpacity(1.0);
-    canvas.drawPath(path0, paint0Fill);
+    Rect rectBlueSquare = Rect.fromLTWH(
+      0,
+      0,
+      size.width * (0.5 - 0.05 - (0.05 / 2)),
+      size.height * (0.5 - 0.05 - 0.1),
+    );
+    Paint rectBlueSquarePaint =
+        Paint()
+          ..style = PaintingStyle.fill
+          ..color = Color(0xff012169);
+    canvas.drawRect(rectBlueSquare, rectBlueSquarePaint);
+
+    /// Bottom left square
+    canvas.drawRect(
+      RectSymmetry.axialSymmetrical(
+        rect: rectBlueSquare,
+        axis: Line(canvasRect.centerLeft, canvasRect.centerRight),
+      ),
+      rectBlueSquarePaint,
+    );
+
+    /// Bottom right square
+    canvas.drawRect(
+      RectSymmetry.centralSymmetrical(rect: rectBlueSquare, center: canvasRect.center),
+      rectBlueSquarePaint,
+    );
+
+    /// Top right square
+    canvas.drawRect(
+      RectSymmetry.axialSymmetrical(
+        rect: RectSymmetry.centralSymmetrical(rect: rectBlueSquare, center: canvasRect.center),
+        axis: Line(canvasRect.centerLeft, canvasRect.centerRight),
+      ),
+      rectBlueSquarePaint,
+    );
 
     Path path1 = Path();
     path1.moveTo(0, 0);
@@ -24,11 +52,11 @@ class GbFlagPainter extends CustomPainter {
         Paint()
           ..style = PaintingStyle.stroke
           ..strokeWidth = size.width * 0.1000000;
-    paint1Stroke.color = Color(0xffffffff).withOpacity(1.0);
+    paint1Stroke.color = Color.fromARGB(255, 255, 255, 255);
     canvas.drawPath(path1, paint1Stroke);
 
     Paint paint1Fill = Paint()..style = PaintingStyle.fill;
-    paint1Fill.color = Color(0xff000000).withOpacity(1.0);
+    paint1Fill.color = Color.fromARGB(255, 0, 0, 0);
     canvas.drawPath(path1, paint1Fill);
 
     Path path2 = Path();
@@ -45,7 +73,7 @@ class GbFlagPainter extends CustomPainter {
     canvas.drawPath(path2, paint2Stroke);
 
     Paint paint2Fill = Paint()..style = PaintingStyle.fill;
-    paint2Fill.color = Color(0xff000000).withOpacity(1.0);
+    paint2Fill.color = Color.fromARGB(255, 0, 0, 0);
     canvas.drawPath(path2, paint2Fill);
 
     Path path3 = Path();
@@ -62,7 +90,7 @@ class GbFlagPainter extends CustomPainter {
     canvas.drawPath(path3, paint3Stroke);
 
     Paint paint3Fill = Paint()..style = PaintingStyle.fill;
-    paint3Fill.color = Color(0xff000000).withOpacity(1.0);
+    paint3Fill.color = Color.fromARGB(255, 0, 0, 0);
     canvas.drawPath(path3, paint3Fill);
 
     Path path4 = Path();
@@ -79,12 +107,12 @@ class GbFlagPainter extends CustomPainter {
     canvas.drawPath(path4, paint4Stroke);
 
     Paint paint4Fill = Paint()..style = PaintingStyle.fill;
-    paint4Fill.color = Color(0xff000000).withOpacity(1.0);
+    paint4Fill.color = Color.fromARGB(255, 0, 0, 0);
     canvas.drawPath(path4, paint4Fill);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
+    return false;
   }
 }
