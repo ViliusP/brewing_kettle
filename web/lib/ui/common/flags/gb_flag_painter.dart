0e8240053b493625394c27d32feb945e2ab7ba21
ViliusP <viliuspaliokas@gmail.com>
2025-04-22 12:45:54 +0300

    Refactor GbFlagPainter

--- web/lib/ui/common/flags/gb_flag_painter.dart
+++ web/lib/ui/common/flags/gb_flag_painter.dart
@@ -6,109 +6,109 @@ class GbFlagPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final Rect canvasRect = Rect.fromLTWH(0, 0, size.width, size.height);
 
-    Rect rectBlueSquare = Rect.fromLTWH(
-      0,
-      0,
-      size.width * (0.5 - 0.05 - (0.05 / 2)),
-      size.height * (0.5 - 0.05 - 0.1),
+    // -----------------------------------
+    // ---------- Blue squares -----------
+    // -----------------------------------
+    final Line horizontalSymmetryLine = Line(canvasRect.centerLeft, canvasRect.centerRight);
+
+    final double whiteCrossHalfWidth = size.width * 0.1666667 / 2;
+
+    final Rect rectBlueSquare = Rect.fromPoints(
+      Offset.zero,
+      Offset(
+        size.width * 0.5,
+        size.height * 0.5,
+      ).translate(-whiteCrossHalfWidth, -whiteCrossHalfWidth),
     );
-    Paint rectBlueSquarePaint =
+
+    final Paint rectBlueSquarePaint =
         Paint()
           ..style = PaintingStyle.fill
-          ..color = Color(0xff012169);
+          ..color = Color.fromRGBO(1, 33, 105, 1);
     canvas.drawRect(rectBlueSquare, rectBlueSquarePaint);
 
-    /// Bottom left square
+    // Bottom left square
     canvas.drawRect(
-      RectSymmetry.axialSymmetrical(
-        rect: rectBlueSquare,
-        axis: Line(canvasRect.centerLeft, canvasRect.centerRight),
-      ),
+      RectSymmetry.axialSymmetrical(rect: rectBlueSquare, axis: horizontalSymmetryLine),
       rectBlueSquarePaint,
     );
 
-    /// Bottom right square
+    // Bottom right square
+    final Rect bottomRightBlueSquare = RectSymmetry.centralSymmetrical(
+      rect: rectBlueSquare,
+      center: canvasRect.center,
+    );
+    canvas.drawRect(bottomRightBlueSquare, rectBlueSquarePaint);
+
+    // Top right square
     canvas.drawRect(
-      RectSymmetry.centralSymmetrical(rect: rectBlueSquare, center: canvasRect.center),
+      RectSymmetry.axialSymmetrical(rect: bottomRightBlueSquare, axis: horizontalSymmetryLine),
       rectBlueSquarePaint,
     );
 
-    /// Top right square
-    canvas.drawRect(
-      RectSymmetry.axialSymmetrical(
-        rect: RectSymmetry.centralSymmetrical(rect: rectBlueSquare, center: canvasRect.center),
-        axis: Line(canvasRect.centerLeft, canvasRect.centerRight),
-      ),
-      rectBlueSquarePaint,
-    );
+    // ------------------------------------
+    // ------- White diagonal lines -------
+    // ------------------------------------
+    final Path whiteDiagonalsPath = Path();
+    whiteDiagonalsPath.moveTo(0, 0);
+    whiteDiagonalsPath.lineTo(size.width, size.height);
+    whiteDiagonalsPath.moveTo(size.width, 0);
+    whiteDiagonalsPath.lineTo(0, size.height);
 
-    Path path1 = Path();
-    path1.moveTo(0, 0);
-    path1.lineTo(size.width, size.height);
-    path1.moveTo(size.width, 0);
-    path1.lineTo(0, size.height);
-
-    Paint paint1Stroke =
+    final Paint whiteDiagonalsPaint =
         Paint()
           ..style = PaintingStyle.stroke
           ..strokeWidth = size.width * 0.1000000;
-    paint1Stroke.color = Color.fromARGB(255, 255, 255, 255);
-    canvas.drawPath(path1, paint1Stroke);
+    whiteDiagonalsPaint.color = Color.fromRGBO(255, 255, 255, 1);
+    canvas.drawPath(whiteDiagonalsPath, whiteDiagonalsPaint);
 
-    Paint paint1Fill = Paint()..style = PaintingStyle.fill;
-    paint1Fill.color = Color.fromARGB(255, 0, 0, 0);
-    canvas.drawPath(path1, paint1Fill);
+    // ------------------------------------
+    // -------- Red diagonal lines --------
+    // ------------------------------------
+    final Path redDiagonalsPath = Path();
+    redDiagonalsPath.moveTo(0, 0);
+    redDiagonalsPath.lineTo(size.width, size.height);
+    redDiagonalsPath.moveTo(size.width, 0);
+    redDiagonalsPath.lineTo(0, size.height);
 
-    Path path2 = Path();
-    path2.moveTo(0, 0);
-    path2.lineTo(size.width, size.height);
-    path2.moveTo(size.width, 0);
-    path2.lineTo(0, size.height);
-
-    Paint paint2Stroke =
+    final Paint redDiagonalsPaint =
         Paint()
           ..style = PaintingStyle.stroke
           ..strokeWidth = size.width * 0.06666667;
-    paint2Stroke.color = Color(0xffC8102E).withOpacity(1.0);
-    canvas.drawPath(path2, paint2Stroke);
+    redDiagonalsPaint.color = Color.fromRGBO(200, 16, 46, 1);
+    canvas.drawPath(redDiagonalsPath, redDiagonalsPaint);
 
-    Paint paint2Fill = Paint()..style = PaintingStyle.fill;
-    paint2Fill.color = Color.fromARGB(255, 0, 0, 0);
-    canvas.drawPath(path2, paint2Fill);
+    // ------------------------------------
+    // ----------- White cross ------------
+    // ------------------------------------
+    final Path whiteCrossPath = Path();
+    whiteCrossPath.moveTo(size.width * 0.5000000, 0);
+    whiteCrossPath.lineTo(size.width * 0.5000000, size.height);
+    whiteCrossPath.moveTo(0, size.height * 0.5000000);
+    whiteCrossPath.lineTo(size.width, size.height * 0.5000000);
 
-    Path path3 = Path();
-    path3.moveTo(size.width * 0.5000000, 0);
-    path3.lineTo(size.width * 0.5000000, size.height);
-    path3.moveTo(0, size.height * 0.5000000);
-    path3.lineTo(size.width, size.height * 0.5000000);
-
-    Paint paint3Stroke =
+    final Paint whiteCrossPaint =
         Paint()
           ..style = PaintingStyle.stroke
           ..strokeWidth = size.width * 0.1666667;
-    paint3Stroke.color = Color(0xffffffff).withOpacity(1.0);
-    canvas.drawPath(path3, paint3Stroke);
+    whiteCrossPaint.color = Color.fromRGBO(255, 255, 255, 1);
+    canvas.drawPath(whiteCrossPath, whiteCrossPaint);
 
-    Paint paint3Fill = Paint()..style = PaintingStyle.fill;
-    paint3Fill.color = Color.fromARGB(255, 0, 0, 0);
-    canvas.drawPath(path3, paint3Fill);
+    // ------------------------------------
+    // ------------ Red cross -------------
+    // ------------------------------------
+    final Path redCrossPath = Path();
+    redCrossPath.moveTo(size.width * 0.5000000, 0);
+    redCrossPath.lineTo(size.width * 0.5000000, size.height);
+    redCrossPath.moveTo(0, size.height * 0.5000000);
+    redCrossPath.lineTo(size.width, size.height * 0.5000000);
 
-    Path path4 = Path();
-    path4.moveTo(size.width * 0.5000000, 0);
-    path4.lineTo(size.width * 0.5000000, size.height);
-    path4.moveTo(0, size.height * 0.5000000);
-    path4.lineTo(size.width, size.height * 0.5000000);
-
-    Paint paint4Stroke =
+    final Paint redCrossPain =
         Paint()
           ..style = PaintingStyle.stroke
           ..strokeWidth = size.width * 0.1000000;
-    paint4Stroke.color = Color(0xffC8102E).withOpacity(1.0);
-    canvas.drawPath(path4, paint4Stroke);
-
-    Paint paint4Fill = Paint()..style = PaintingStyle.fill;
-    paint4Fill.color = Color.fromARGB(255, 0, 0, 0);
-    canvas.drawPath(path4, paint4Fill);
+    redCrossPain.color = Color.fromRGBO(200, 16, 46, 1);
+    canvas.drawPath(redCrossPath, redCrossPain);
   }
 
   @override
