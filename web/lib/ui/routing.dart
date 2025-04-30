9f4fbb081a15f98057b81866bed39be11b1c28b9
ViliusP <viliuspaliokas@gmail.com>
2025-04-30 13:13:50 +0300

    Add routing transitions

--- web/lib/ui/routing.dart
+++ web/lib/ui/routing.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:brew_kettle_dashboard/core/data/models/websocket/connection_status.dart';
 import 'package:brew_kettle_dashboard/core/service_locator.dart';
 import 'package:brew_kettle_dashboard/stores/websocket_connection/websocket_connection_store.dart';
@@ -11,7 +12,6 @@ import 'package:brew_kettle_dashboard/ui/screens/not_found_404/not_found_404_scr
 import 'package:brew_kettle_dashboard/ui/screens/settings/settings_screen.dart';
 import 'package:brew_kettle_dashboard/ui/screens/test/test_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
@@ -53,11 +53,9 @@ class AppRouter {
       navigatorKey: _rootNavigatorKey,
       initialLocation: AppRoute.connection.path,
       debugLogDiagnostics: true,
-
       errorBuilder: (context, state) {
         return NotFound404Screen();
       },
-
       routes: [
         ShellRoute(
           navigatorKey: _defaultLayoutNavigatorKey,
@@ -69,11 +67,13 @@ class AppRouter {
               path: AppRoute.information.path,
               name: AppRoute.information.name,
               builder: (context, state) => InformationScreen(),
+              pageBuilder: _defaultPageBuilder(InformationScreen()),
             ),
             GoRoute(
               path: AppRoute.connection.path,
               name: AppRoute.connection.name,
               builder: (context, state) => ConnectionScreen(),
+              pageBuilder: _defaultPageBuilder(ConnectionScreen()),
             ),
             ShellRoute(
               navigatorKey: _connectedLayout,
@@ -95,21 +95,27 @@ class AppRouter {
               builder: (BuildContext context, GoRouterState state, Widget child) {
                 return ConnectedLayout(child);
               },
+              pageBuilder: (context, state, child) {
+                return _defaultPageBuilder(ConnectedLayout(child))(context, state);
+              },
               routes: <RouteBase>[
                 GoRoute(
                   path: AppRoute.main.path,
                   name: AppRoute.main.name,
                   builder: (context, state) => const MainScreen(),
+                  pageBuilder: _conntectedPageBuilder(const MainScreen()),
                 ),
                 GoRoute(
                   path: AppRoute.settings.path,
                   name: AppRoute.settings.name,
                   builder: (context, state) => SettingsScreen(),
+                  pageBuilder: _conntectedPageBuilder(SettingsScreen()),
                 ),
                 GoRoute(
                   path: AppRoute.devices.path,
                   name: AppRoute.devices.name,
                   builder: (context, state) => DevicesScreen(),
+                  pageBuilder: _conntectedPageBuilder(DevicesScreen()),
                 ),
                 GoRoute(
                   path: AppRoute.test.path,
@@ -124,3 +130,58 @@ class AppRouter {
     );
   }
 }
+
+CustomTransitionPage<T> _buildPageWithDefaultTransition<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) {
+  return CustomTransitionPage<T>(
+    transitionDuration: Durations.short4,
+    reverseTransitionDuration: Durations.short4,
+    key: state.pageKey,
+    child: child,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return FadeThroughTransition(
+        animation: animation,
+        secondaryAnimation: secondaryAnimation,
+        child: child,
+      );
+    },
+  );
+}
+
+Page<T> Function(BuildContext, GoRouterState) _defaultPageBuilder<T>(Widget child) => (
+  BuildContext context,
+  GoRouterState state,
+) {
+  return _buildPageWithDefaultTransition<T>(context: context, state: state, child: child);
+};
+
+CustomTransitionPage<T> _buildPageWithVerticalTransition<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) {
+  return CustomTransitionPage<T>(
+    transitionDuration: Durations.short4,
+    reverseTransitionDuration: Durations.short4,
+    key: state.pageKey,
+    child: child,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return SharedAxisTransition(
+        animation: animation,
+        secondaryAnimation: secondaryAnimation,
+        transitionType: SharedAxisTransitionType.vertical,
+        child: child,
+      );
+    },
+  );
+}
+
+Page<T> Function(BuildContext, GoRouterState) _conntectedPageBuilder<T>(Widget child) => (
+  BuildContext context,
+  GoRouterState state,
+) {
+  return _buildPageWithVerticalTransition<T>(context: context, state: state, child: child);
+};
